9253858c850ce6ac226e284f9d49e2f5cfa17540
Tiryoh <tiryoh@gmail.com>
2017-02-11 21:45:03 +0900

    Fix talker.py run error

--- scripts/talker.py
+++ scripts/talker.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymouse=True)
+    rospy.init_node('talker', anonymous=True)
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
         str = "hello world %s"%rospy.get_time()
