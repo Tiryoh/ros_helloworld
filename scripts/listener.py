abdce2e5aa651d988bcc453f13fa74275dd217ea
Tiryoh <tiryoh@gmail.com>
2017-02-11 21:50:40 +0900

    Fix listener run error

--- scripts/listener.py
+++ scripts/listener.py
@@ -8,8 +8,8 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s",data.data)
 
 def listener():
-    rospy.init_node('listener', anonymouse=True)
-    rospy.subscriber("chatter", String, callback)
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("chatter", String, callback)
     rospy.spin()
 
 if __name__ == '__main__':
